863f53f327d18b57a31aa7c9d1e401c36e153e50
Bhaskar Dutta <61713203+bworkspace@users.noreply.github.com>
2020-09-17 15:01:09 +0530

    Create Student.cs

---
+++ Student.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentService.Model
+{
+    public class Student
+    {
+        public int Id { get; set; };
+        public string name { get; set; };
+        public int rollno { get; set; };
+    }
+}
